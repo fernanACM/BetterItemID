c03a3e8e40f721a9be9346ef8795b4a460a59e98
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-05-07 22:29:46 -0500

    Perfect!

---
+++ src/fernanACM/BetterItemID/ItemID.php
@@ -0,0 +1,47 @@
+<?php
+    
+namespace fernanACM\BetterItemID;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+use pocketmine\item\Item;
+
+use pocketmine\event\player\PlayerItemHeldEvent;
+
+use pocketmine\permission\DefaultPermissions;
+
+use pocketmine\utils\Config;
+
+use fernanACM\BetterItemID\utils\PluginUtils;
+use fernanACM\BetterItemID\commands\IdCommand;
+
+class ItemID extends PluginBase implements Listener {
+    
+    public Config $config;
+        
+	public function onEnable(): void{
+        $this->saveDefaultConfig();
+		$this->saveResource("config.yml");
+		$this->config = new Config($this->getDataFolder() . "config.yml");
+        $this->getServer()->getPluginManager()->registerEvents($this ,$this);
+        $this->getServer()->getCommandMap()->register("id", new IdCommand($this));
+    }
+        
+    public function ItemHeld(PlayerItemHeldEvent $event){
+        if($this->config->get("Settings")["No-tip-itemid"]){
+            $player = $event->getPlayer();
+            if($player->hasPermission(DefaultPermissions::ROOT_OPERATOR)){
+                $message = $this->getMessage($player, "Messages.Tip-itemid");
+                $player->sendTip(str_replace(["{ID}", "{META}"], [$event->getItem()->getId(), $event->getItem()->getMeta()], $message));
+            }
+        }
+    }
+    
+    public function getMessage(Player $player, string $key){
+        return PluginUtils::codeUtil($player, $this->config->getNested($key, $key));
+    }
+}
---
+++ src/fernanACM/BetterItemID/commands/IdCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace fernanACM\BetterItemID\commands;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\Plugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+
+use fernanACM\BetterItemID\ItemID;
+use fernanACM\BetterItemID\utils\PluginUtils;
+
+class IdCommand extends Command implements PluginOwned{
+    
+    private $plugin;
+
+    public function __construct(ItemID $plugin){
+        $this->plugin = $plugin;
+        
+        parent::__construct("id", "§r§fItemID by fernanACM", "§cUse: /id | itemid", ["itemid"]);
+        $this->setPermission("item.acm");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(count($args) == 0){
+            if($sender instanceof Player) {
+                $item = $sender->getInventory()->getItemInHand();
+                $message = $this->plugin->getMessage($sender, "Messages.Itemid");
+                $message = str_replace('{ID}', $item->getId(), $message);
+                $message = str_replace('{META}', $item->getMeta(), $message);
+                $message = str_replace('{BLOCK-NAME}', $item->getName(), $message);
+                if($this->plugin->config->get("Settings")["Id-no-sound"]){
+                   PluginUtils::PlaySound($sender, $this->plugin->config->get("Settings")["Id-sound"], 1, 3);
+                }
+                $sender->sendMessage($message);
+                }else{
+                   $sender->sendMessage("Use this command in-game");
+            }
+        }
+        return true;
+    }
+    
+    public function getPlugin(): Plugin{
+        return $this->plugin;
+    }
+
+    public function getOwningPlugin(): ItemID{
+        return $this->plugin;
+    }
+}
