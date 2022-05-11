e2089ac5169dbc24253b41c73dea77b299561689
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-05-10 22:13:22 -0500

    New version 1.0.1: Fixed permission

--- src/fernanACM/BetterItemID/commands/BetterItemIdCommand.php
+++ src/fernanACM/BetterItemID/commands/BetterItemIdCommand.php
@@ -8,46 +8,31 @@ use pocketmine\plugin\Plugin;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\PluginOwned;
-
+# Lib - Commando
+use CortexPE\Commando\BaseCommand;
+# My files
 use fernanACM\BetterItemID\ItemID;
 use fernanACM\BetterItemID\utils\PluginUtils;
 
-class BetterItemIdCommand extends Command implements PluginOwned{
+class BetterItemIdCommand extends BaseCommand{
     
-    private $plugin;
-
-    public function __construct(ItemID $plugin){
-        $this->plugin = $plugin;
-        
-        parent::__construct("betteritemid", "BetterItemID by fernanACM", "§cUse: /betteritemid | itemid | id", ["itemid", "id"]);
+    protected function prepare(): void{
         $this->setPermission("betteritemid.cmd.acm");
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args){
-        if(count($args) == 0){
-            if($sender instanceof Player) {
-                $item = $sender->getInventory()->getItemInHand();
-                $message = $this->plugin->getMessage($sender, "Messages.Itemid");
-                $message = str_replace('{ID}', $item->getId(), $message);
-                $message = str_replace('{META}', $item->getMeta(), $message);
-                $message = str_replace('{BLOCK-NAME}', $item->getName(), $message);
-                if($this->plugin->config->get("Settings")["Id-no-sound"]){
-                   PluginUtils::PlaySound($sender, $this->plugin->config->get("Settings")["Id-sound"], 1, 3);
-                }
-                $sender->sendMessage($message);
-                }else{
-                   $sender->sendMessage("Use this command in-game");
-            }
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void{
+        if (!$sender instanceof Player) {
+              $sender->sendMessage("Use this command in-game");
+               return;
         }
-        return true;
-    }
-    
-    public function getPlugin(): Plugin{
-        return $this->plugin;
-    }
-
-    public function getOwningPlugin(): ItemID{
-        return $this->plugin;
+        $item = $sender->getInventory()->getItemInHand();
+        $message = ItemID::getInstance()->getMessage($sender, "Messages.Itemid");
+        $message = str_replace('{ID}', $item->getId(), $message);
+        $message = str_replace('{META}', $item->getMeta(), $message);
+        $message = str_replace('{BLOCK-NAME}', $item->getName(), $message);
+        if(ItemID::getInstance()->config->get("Settings")["Id-no-sound"]){
+            PluginUtils::PlaySound($sender, ItemID::getInstance()->config->get("Settings")["Id-sound"], 1, 3);
+        }
+        $sender->sendMessage($message);
     }
 }
